fbd94bfed31ec8618d5e8798f18b35b37891fa58
GevorHakobyan <gevorghakobyan609@gmail.com>
2024-09-21 16:39:10 +0400

    deleted unnecessary files

--- myLibrary/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vector.h"
-#include <iostream>
-
-int main() {
-    geometry::Vector a{3, 4, 0};
-    geometry::Vector b{1, 2, 0};
-    std::cout << a.ProjectionOn_Vector(b).getX() << "\n";
-}
